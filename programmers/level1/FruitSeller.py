a2142f90179a1710986d99dd3bd04747281e566d
birdieHyun <flaehdan@gmail.com>
2023-02-21 21:38:43 +0900

    feat : DFS/BFS

---
+++ programmers/level1/FruitSeller.py
@@ -0,0 +1,18 @@
+def solution(k, m, score):
+    answer = 0
+
+    score.sort(reverse=True)
+    print(score)
+
+
+    # for i in range(len(score // m)):
+
+
+
+    return answer
+
+score = [1, 2, 3, 1, 2, 3, 1]
+
+print(4 % 3)
+
+# print(solution(1,2, score))
--- thisiscodingtest/dfsbfs/Recursion_Practice.py
+++ thisiscodingtest/dfsbfs/Recursion_Practice.py
@@ -1,19 +1,22 @@
-# def recursion(i):
-#     if i == 100:
-#         return
-#     print(i, '번째 함수에서 ', i+1, '번째 함수를 호출합니다')
-#     recursion(i+1)
-#     print(i, '번째 함수를 종료합니다')
-#
-# recursion(1)
+def recursion(i):
+    if i == 100:
+        return
+    print(i, '번째 함수에서 ', i + 1, '번째 함수를 호출합니다')
+    recursion(i + 1)
+    print(i, '번째 함수를 종료합니다')
+
+
+recursion(1)
+
 
 # 두 가지 방식으로 구현한 팩토리얼 예제
 
 def factorial_1(n):
     result = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         result *= i
     return result
+
 
 print(factorial_1(5))
 
@@ -21,6 +24,7 @@ print(factorial_1(5))
 def factorial_2(n):
     if n <= 1:
         return 1
-    return n * factorial_2(n-1)
+    return n * factorial_2(n - 1)
+
 
 print(factorial_2(5))
