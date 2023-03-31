5110b0dc81256cb688e674d06b4abe6fe26d84bf
birdieHyun <flaehdan@gmail.com>
2023-03-31 10:08:45 +0900

    Feat : 알고리즘

--- programmers/level1/Kakao2022.py
+++ programmers/level1/Kakao2022.py
@@ -1,32 +1,20 @@
+# multi value set 만들어서 신고당한 회원(key) - 신고한 회원(value) 로 저장
+from collections import defaultdict
+
 def solution(id_list, report, k):
-    answer = []
-    reported = []
-    id_dic = {}
+    answer = [0] * len(id_list)  # 0번 안내를 받는 것으로 초기화
 
-    for i in range(len(id_list)):
-        id_dic[id_list[i]] = 0
+    multi_map = defaultdict(set) # 하나의 키에 여러 value를 중복으로 받지 않는 map 생성
 
-    for i in range(len(report)):
-        list = report[i].split(" ")
-        if report[i] not in reported:
-            reported.append(report[i])
-            id_dic[list[1]] += 1
+    for r in report:
+        r = r.split()
+        multi_map[r[1]].add(r[0])   # 신고당한 회원 (key) - 신고한 회원(value)
 
-    for i in range(len(id_list)):
-        if id_dic[id_list[i]] >= k:
-            answer.append(id_dic[id_list[i]] + 1 - k)
-        else:
-            answer.append(0)
+    for i in (multi_map):
+        if len(multi_map[i]) >= k:     # 신고한 회원 >= k 일 경우 -> 신고한 회원의 알림 횟수 +1
+            for person in multi_map[i]:
+                index = id_list.index(person)
+                answer[index] += 1
+
 
     return answer
-
-
-id_list = ["muzi", "frodo", "apeach", "neo"]
-report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
-k = 2
-
-# id_list = ["con", "ryan"]
-# report = ["ryan con", "ryan con", "ryan con", "ryan con"]
-# k = 3
-
-print(solution(id_list, report, k))
