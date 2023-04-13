9565e366de687c6815bca7298eb651fb6d18cb22
birdieHyun <flaehdan@gmail.com>
2023-04-13 19:54:27 +0900

    알고리즘

---
+++ programmers/lv2/LongJump.py
@@ -0,0 +1,16 @@
+def solution(n):
+    if n < 3:
+        return n
+
+    d = [0] * (n + 1)
+    d[1] = 1
+    d[2] = 2
+
+    for i in range(3, n + 1):
+        d[i] = d[i - 1] + d[i - 2]
+
+    return d[n] % 1234567
+
+# 1234567 나머지 출력하는 프로그램
+n = 4
+print(solution(n))
