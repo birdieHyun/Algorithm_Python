e5b39c011dedecaad11060d9a59879629e804a34
birdieHyun <flaehdan@gmail.com>
2023-05-10 08:23:03 +0900

    알고리즘

---
+++ thisiscodingtest/sorting/TwoArrayChange.py
@@ -0,0 +1,17 @@
+size, count = map(int, input().split())
+
+first = list(map(int, input().split()))
+
+second = list(map(int, input().split()))
+
+first.sort()
+second.sort(reverse=True)
+
+for i in range(count):
+    if first[i] >= second[i]:
+        first[i], second[i] = second[i], first[i]
+    else:
+        break
+
+print(sum(first))
+
