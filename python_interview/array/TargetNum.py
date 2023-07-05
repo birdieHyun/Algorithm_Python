aea4a941d76fcb443ff7cf1153c8fcf77a85a2b3
birdieHyun <flaehdan@gmail.com>
2023-07-06 08:23:08 +0900

    알고리즘

--- python_interview/array/TargetNum.py
+++ python_interview/array/TargetNum.py
@@ -9,3 +9,18 @@ def find_target(numbers, target_num):
                 return [i, j]
 
 print(find_target(nums, target))
+
+# 투 포인터 이용 -> 그러나 이렇게 되면 인덱스가 섞여버리기 때문에 좋은 방법은 아니다. 투포인터는 정렬된 상황에서 사용하도록 하자
+
+def find_target2(numbers, target_num):
+    first, last = 0, len(numbers) - 1
+    while first < last:
+        if numbers[first] + numbers[last] == target_num:
+            return [first, last]
+        elif numbers[first] + numbers[last] > target_num :
+            last -= 1
+        else:
+            first += 1
+
+print(find_target2(nums, target))
+
