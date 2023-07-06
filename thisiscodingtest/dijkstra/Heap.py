02f4123bcde3137c2cec00c43a38a20ab7b9c477
birdieHyun <flaehdan@gmail.com>
2023-07-07 08:17:26 +0900

    feat: 최대힙 공부

--- thisiscodingtest/dijkstra/Heap.py
+++ thisiscodingtest/dijkstra/Heap.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def heapsort(iterable):
     h = []
     result = []
@@ -13,5 +14,24 @@ def heapsort(iterable):
         result.append(heapq.heappop(h))
     return result
 
+
 result = heapsort([1, 3, 4, 7, 9, 2, 4, 6, 8, 0])
 print(result)
+
+
+## 최대 힙을 구현하고 싶다면?
+def maxheapsort(iterable):
+    h = []
+    result = []
+
+    for value in iterable:
+        heapq.heappush(h, -value)
+
+    for i in range(len(h)):
+        result.append(-heapq.heappop(h))
+
+    return result
+
+
+answer = maxheapsort([1, 3, 4, 7, 9, 2, 4, 6, 8, 0])
+print(answer)
