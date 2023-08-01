dd64db2f859d3243c0790a19c14fc34db127e541
birdieHyun <flaehdan@gmail.com>
2023-08-01 23:25:29 +0900

    feat: 백준 11286

--- baekjoon/gold/gold5/G21610.py
+++ baekjoon/gold/gold5/G21610.py
@@ -1,1 +1,9 @@
-# 1 부터 8까지 시계방향으로 회전 
+n, m = map(int, input().split())
+
+water_map = [list(map(int, input().split())) for _ in range(n)]
+cloud_map = [[False] * n for _ in range(n)]
+print(cloud_map)
+
+# m번 만큼의 이동
+for _ in range(m):
+    direction, distance = map(int, input().split())
