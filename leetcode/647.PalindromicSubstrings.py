80c9ccf360b7f97ae8e4dabd06545bfb84c7bd47
birdie <flaehdan@gmail.com>
2023-12-24 09:40:13 +0900

    feat: 리트코드

---
+++ leetcode/647.PalindromicSubstrings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        answer = []
+        for i in s:
+            answer.append(i)
+
+        def expand(left, right):
+
+            while left > 0 and right < len(s) - 1 and s[left] == s[right]:
+                if s[left] == s[right]:
+                    answer.append(s[left: right + 1])
+
+                left -= 1
+                right += 1
+
+
+            if s[left] == s[right]:
+                answer.append(s[left : right + 1])
+
+        for i in range(1, len(s) - 1):
+            expand(i - 1, i + 1)
+
+        for i in range(1, len(s)):
+            expand(i - 1, i)
+
+        print(answer)
+        print(len(answer))
+        return len(answer)
+
+s = Solution()
+s.countSubstrings("hello")
