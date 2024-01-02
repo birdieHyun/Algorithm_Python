581970dc64c9d73010940772e5a95e814c8f3e30
birdie <flaehdan@gmail.com>
2024-01-02 19:56:07 +0900

    feat: 다익스트라 학습

--- thisiscodingtest/dijkstra/dijkstra3.py
+++ thisiscodingtest/dijkstra/dijkstra3.py
@@ -1,25 +1,21 @@
 import heapq
 
-INF = int(1e9)
+INF = int(10e9)
 
-# 노드의 개수 간선의 개수 입력받기
 n, m = map(int, input().split())
 
-# 시작점 입력받기
 start = int(input())
 
 graph = [[] for _ in range(n + 1)]
 
 distance = [INF] * (n + 1)
 
-
-# 그래프간 연결정보 입력
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
-
 def dijkstra(start):
+
     q = []
 
     heapq.heappush(q, (0, start))
@@ -31,11 +27,9 @@ def dijkstra(start):
         if distance[now] < dist:
             continue
 
-        # 탐색하지 않았더라면 연결된 노드들 탐색
         for i in graph[now]:
             cost = dist + i[1]
 
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
-
