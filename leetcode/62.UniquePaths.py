e57465987f7709d96f2e04fbd4237559484873ba
birdie <flaehdan@gmail.com>
2024-01-23 16:58:56 +0900

    feat: leetcode

---
+++ leetcode/62.UniquePaths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        # 세로 = m, 가로 = n
+
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[-1][-1]
+
+s = Solution()
+s.uniquePaths(3, 7)
