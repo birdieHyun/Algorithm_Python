6f0b7aa7330c8cb48f82538b71361fece71485e0
birdie <flaehdan@gmail.com>
2024-02-13 08:49:17 +0900

    feat: 알고리즘

---
+++ leetcode/57.InsertInterval_2.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        left, right = [], []
+
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                left.append(interval)
+            elif interval[0] > newInterval[1]:
+                right.append(interval)
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+
+        return left + [newInterval] + right
